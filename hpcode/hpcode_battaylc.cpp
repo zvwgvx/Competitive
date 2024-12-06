bb73d40245059e977192782ba6eb1283843adb78
zvwgvx <orzitzzang@gmail.com>
2024-12-06 17:46:04 +0700

    Upload files to GitHub

--- hpcode/hpcode_battaylc.cpp
+++ hpcode/hpcode_battaylc.cpp
@@ -3,6 +3,7 @@
 #pragma GCC optimize("fast-math")
 #pragma GCC optimize("no-stack-protector") 
 #pragma GCC optimize("Ofast,unroll-loops")
+#pragma GCC target("avx,avx2,fma")
 
 #include <bits/stdc++.h>
 
@@ -15,34 +16,6 @@ using namespace std;
 #define f(array) (array).begin(), (array).end()
 #define b(array) (array).rbegin(), (array).rend()
 
-const ll MAXQ = 1e6;
- 
-bitset<MAXQ> sieve(int N) {
-    bitset<MAXQ> is_prime; 
-
-    is_prime.set(); 
-
-    is_prime[0] = is_prime[1] = 0;
-
-    for (int i = 2; i * i <= N; ++i) {
-        if (is_prime[i]) {
-            for (int j = i * i; j <= N; j += i) {
-                is_prime[j] = 0; 
-            }
-        }
-    }
-    return is_prime;
-} 
-
-ll reve(ll N) {
-    ll rev = 0;
-    while (N > 0) {
-        rev = rev * 10 + N % 10;
-        N /= 10;
-    }
-    return rev;
-}
-
 signed main() { 
     cin.tie(nullptr), cout.tie(nullptr) 
     -> ios_base::sync_with_stdio(false);
@@ -53,16 +26,19 @@ signed main() {
             freopen (file ".out", "w", stdout);
     #endif  
 
-    ll left, right;
+    ll N;
+    
+    cin >> N;
 
-    cin >> left >> right;
-
-    bitset <MAXQ> is_prime = sieve(MAXQ);
-
-    for (int i = left; i <= right; ++i) {
-        if (is_prime[reve(i)]) {
-            cout << i << '\n';
-        }
+    if (N < 2) {
+        cout << 0;
+        return 0;
+    } else if (N == 2) {
+        cout << 1;
+        return 0;
+    } else {
+        cout << N * (N - 1) / 2;
+        return 0;
     }
     return 0;   
 }
