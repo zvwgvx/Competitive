9e05cd5d6f35926ca414c81e9e1f5829cdfb250c
zvwgvx <orzitzzang@gmail.com>
2024-12-06 11:16:29 +0700

    Upload files to GitHub

--- main.cpp
+++ main.cpp
@@ -1,47 +1,52 @@
-#define _CRT_SECURE_NO_WARNINGS
-#define _CRT_DISABLE_PERFCRIT_LOCKS
-#pragma GCC optimize("fast-math")
-#pragma GCC optimize("no-stack-protector") 
-#pragma GCC optimize("Ofast,unroll-loops")
-#pragma GCC target("avx,avx2,fma")
-
-#include <bits/stdc++.h>
-
+#include <iostream>
+#include <vector>
+#include <unordered_map>
 using namespace std;
 
-#define v vector
-#define ll long long
-#define umap unordered_map
-#define uset unordered_set
-#define f(array) (array).begin(), (array).end()
-#define b(array) (array).rbegin(), (array).rend()
+void solve() {
+    int n, k;
+    cin >> n >> k;
 
-double am_calc (const v <ll> prefix, ll left, ll right) {
-    return (double)(prefix[right] - prefix[left]) / (right - left + 1);
-}
-
-signed main() { 
-    cin.tie(nullptr), cout.tie(nullptr) 
-    -> ios_base::sync_with_stdio(false);
-
-    #define file "main"
-    #ifndef ONLINE_JUDGE
-            freopen (file ".inp", "r", stdin);
-            freopen (file ".out", "w", stdout);
-    #endif  
-
-    ll N;
-
-    cin >> N;
-    
-    v <ll> array(N), prefix(N + 1, 0);
-
-    for (int i = 0; i < N; ++i) {
-        cin >> array[i];
-        prefix[i] = prefix[i - 1] + array[i];    
+    vector<int> a(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> a[i];
     }
 
-    
+    unordered_map<int, int> prefix_map;
+    int prefix_sum = 0;
+    int max_length = 0;
+    int start_index = -1;
 
-    return 0;   
+    prefix_map[0] = -1;
+
+    for (int i = 0; i < n; ++i) {
+        prefix_sum += a[i] - k;
+
+        if (prefix_map.find(prefix_sum) != prefix_map.end()) {
+            int prev_index = prefix_map[prefix_sum];
+            int length = i - prev_index;
+            if (length > max_length) {
+                max_length = length;
+                start_index = prev_index + 2;
+            }
+        } else {
+            prefix_map[prefix_sum] = i;
+        }
+    }
+
+    if (max_length > 0) {
+        cout << max_length << " " << start_index << endl;
+    } else {
+        cout << 0 << endl;
+    }
+}
+int main() {
+
+    #ifndef ONLINE_JUDGE
+            freopen ("main.inp", "r", stdin);
+            freopen ("main.out", "w", stdout);
+    #endif
+
+    solve();
+    return 0;
 }
